512ae1139e6402ddc8827494997bc99456954541
alessandro <alessandrodamata6@gmail.com>
2024-01-03 19:28:38 -0300

    feat: implementando interface CreditService save, findByCreditCode e findAllCustomer

---
+++ src/main/kotlin/com/cenna/creditapplicationsystem/service/impl/CreditService.kt
@@ -0,0 +1,34 @@
+package com.cenna.creditapplicationsystem.service.impl
+
+import com.cenna.creditapplicationsystem.entity.Credit
+import com.cenna.creditapplicationsystem.repository.CreditRepository
+import com.cenna.creditapplicationsystem.service.ICreditService
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CreditService(
+    private val creditRepository: CreditRepository,
+    private val customerService: CustomerService
+): ICreditService {
+    override fun save(credit: Credit): Credit {
+        credit.apply {
+            customer = customerService.findById(credit.customer?.id!!)
+        }
+       return this.creditRepository.save(credit)
+    }
+
+    override fun findAllByCustomer(customerId: Long): List<Credit> {
+       return this.creditRepository.findAllByCustomerID(customerId)
+    }
+
+    override fun findByCreditCode(customerId: Long, creditCode: UUID): Credit {
+        val credit: Credit = (this.creditRepository.findByCreditCode(creditCode) ?: throw RuntimeException("CreditCode $creditCode não existe."))
+
+        return if(credit.customer?.id == customerId){
+            credit
+        } else {
+            throw RuntimeException("Contate o adimistrador.")
+        }
+    }
+}
