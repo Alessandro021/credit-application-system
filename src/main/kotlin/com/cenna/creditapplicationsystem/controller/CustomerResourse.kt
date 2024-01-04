448ae45ab62242deb86456e4d73f776f3c183806
alessandro <alessandrodamata6@gmail.com>
2024-01-04 16:18:31 -0300

    feat: criando endpoints CustomerResource findById e deleteCustomer

--- src/main/kotlin/com/cenna/creditapplicationsystem/controller/CustomerResourse.kt
+++ src/main/kotlin/com/cenna/creditapplicationsystem/controller/CustomerResourse.kt
@@ -1,7 +1,12 @@
 package com.cenna.creditapplicationsystem.controller
 
 import com.cenna.creditapplicationsystem.dto.CustomerDto
+import com.cenna.creditapplicationsystem.dto.CustomerView
+import com.cenna.creditapplicationsystem.entity.Customer
 import com.cenna.creditapplicationsystem.service.impl.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -17,5 +22,17 @@ class CustomerResourse(
        val saveCustomer =  this.customerService.save(customerDto.toEntity())
 
         return "Cliente ${saveCustomer.email} salvo com sucesso."
+    }
+
+    @GetMapping("/id")
+    fun findById(@PathVariable id: Long): CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/id")
+    fun deleteCustomer(@PathVariable id: Long){
+        return this.customerService.delete(id)
     }
 }
