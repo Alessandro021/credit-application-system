145170b6f44fd2d23b74f387c873c26b0616ae7b
alessandro <alessandrodamata6@gmail.com>
2024-01-04 17:21:07 -0300

    feat: criando endpoints CreditResource findByCreditCode

--- src/main/kotlin/com/cenna/creditapplicationsystem/controller/CreditResource.kt
+++ src/main/kotlin/com/cenna/creditapplicationsystem/controller/CreditResource.kt
@@ -2,14 +2,17 @@ package com.cenna.creditapplicationsystem.controller
 
 import com.cenna.creditapplicationsystem.dto.CreditDto
 import com.cenna.creditapplicationsystem.dto.CreditListView
+import com.cenna.creditapplicationsystem.dto.CreditView
 import com.cenna.creditapplicationsystem.entity.Credit
 import com.cenna.creditapplicationsystem.service.impl.CreditService
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.UUID
 import java.util.stream.Collectors
 
 @RestController
@@ -26,6 +29,13 @@ class CreditResource(
     @GetMapping
     fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditListView> {
         return this.creditService.findAllByCustomer(customerId).stream().map { credit: Credit -> CreditListView(credit) }.collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(@RequestParam(value = "customerId") customerId: Long, @PathVariable creditCode: UUID ): CreditView {
+        val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+
+        return CreditView(credit)
     }
 
 }
