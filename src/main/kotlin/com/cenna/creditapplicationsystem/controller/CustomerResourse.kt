a146aa6e839cbb80fd330f7cdeb5dacb4aedd457
alessandro <alessandrodamata6@gmail.com>
2024-01-04 16:34:55 -0300

    feat: criando endpoints CustomerResource update

--- src/main/kotlin/com/cenna/creditapplicationsystem/controller/CustomerResourse.kt
+++ src/main/kotlin/com/cenna/creditapplicationsystem/controller/CustomerResourse.kt
@@ -1,15 +1,18 @@
 package com.cenna.creditapplicationsystem.controller
 
 import com.cenna.creditapplicationsystem.dto.CustomerDto
+import com.cenna.creditapplicationsystem.dto.CustomerUpdateDto
 import com.cenna.creditapplicationsystem.dto.CustomerView
 import com.cenna.creditapplicationsystem.entity.Customer
 import com.cenna.creditapplicationsystem.service.impl.CustomerService
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -34,5 +37,14 @@ class CustomerResourse(
     @DeleteMapping("/id")
     fun deleteCustomer(@PathVariable id: Long){
         return this.customerService.delete(id)
+    }
+
+    @PatchMapping
+    fun updateCustomer(@RequestParam(value = "customerId") id: Long, @RequestBody customerUpdateDto: CustomerUpdateDto): CustomerView {
+       val customer: Customer =  this.customerService.findById(id)
+       val customerToUpdate: Customer =  customerUpdateDto.toEntity(customer)
+       val customerUpdate: Customer = this.customerService.save(customerToUpdate)
+
+       return CustomerView(customerUpdate)
     }
 }
